5d7afb57e999e8f5f859482234a0c03d27cb4071
laraibjahangir-spec <laraib.jahangir@gmail.com>
2026-05-05 15:50:57 +0500

    add CI pipeline

--- utils/validator.py
+++ utils/validator.py
@@ -7,18 +7,4 @@ def validate_response(response):
         assert "userId" in item
         assert "id" in item
         assert "title" in item
-        assert "body" in item
-
-#def validate_single_post(response , post_no=None):
-#        data = response.json()
-#        assert len(data) > 0
-#
-#        if isinstance(data , list):
-#            post = next(items for item in data if item["id"] == post_id , None)
-#        else:
-#            post = data
-#        assert "id" in 
-            
-        
-
-            
+        assert "body" in item          
